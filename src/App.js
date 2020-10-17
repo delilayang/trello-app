6fd385f1496db7e44230bb6c13f40abeeb173e21
delila <delila@sinitic.ai>
2020-10-17 12:24:28 +0800

    implement trello app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="catagory">
+          <div className="catagory-title">To Do</div>
+          <div className="catagory-item">Gorceries shopping</div>
+        </div>
+        <div className="create-catagory">
+          <input type="text" value="" placeholder="enter catagory item" />
+        </div>
+      </div>
     </div>
   );
 }
